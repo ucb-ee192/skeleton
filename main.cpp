16377817acb6e657d6a24e7e5c90f88d502072c3
ronf <ronf@eecs.berkeley.edu>
2016-01-19 18:17:22 -0800

    add SysTick reading

--- main.cpp
+++ main.cpp
@@ -8,6 +8,16 @@ DigitalOut led_green(LED_GREEN);
 DigitalOut led_red(LED_RED);
 PwmOut led_blue(LED_BLUE);
 
+volatile uint32_t msTicks;                            /* counts 1ms timeTicks */
+volatile static int delay=0;
+/*----------------------------------------------------------------------------
+  SysTick_Handler
+ *----------------------------------------------------------------------------*/
+void SysTick_Handler(void) {
+    msTicks++;                        /* increment counter necessary in Delay() */     
+    delay++;
+}
+
 void led_fade_thread(void const *args) {
   // Note that this function doesn't terminate, which is fine since it runs in
   // a thread.
@@ -33,10 +43,14 @@ void putchar_thread(void const *args)
 }
 
 void putchar_thread1(void const *args)
-{   char c= 'y';
+{   char c= 'y'; 
+	//long value;
 	while(1)
 	{ Thread::wait(1001);
 		serial.putc(c);
+//		value = putcharThread1.get_state();
+		serial.printf("putcharThread1: ");
+	//  print_thread_status(value1);
 	}
 }
 
@@ -46,19 +60,26 @@ void led_blink_periodic(void const *args) {
 }
 
 
-//void print_thread_status(long value)
-//{
-//		switch(value)
-//		{ case 0: serial.putc('0');
-//		  case 1: serial.putc('R');
-//			default: serial.putc('?');
-//		
-//		}
-//}
+void print_thread_status(long value)
+{
+		switch(value)
+		{ case 0: serial.printf("Inactive "); break;
+		  case 1: serial.printf("Ready "); break;
+			case 2: serial.printf("Running "); break;
+			case 3: serial.printf("WaitingDelay "); break;
+			case 4: serial.printf("WaitingInterval "); break;
+			default: serial.putc('?');
+		
+		}
+}
+
+
+
 
 int main() {
 	long value1;
-	//long value2;
+	long value2;
+	long ticks;
 	
   // It's always nice to know what version is deployed.
   serial.printf("Built " __DATE__ " " __TIME__ "\r\n");
@@ -71,7 +92,8 @@ int main() {
   
   // Mandatory "Hello, world!".
   serial.printf("Hello, world!\r\n");
-
+	ticks = SysTick->VAL;
+  serial.printf("starting time %ld |", ticks);
   // Start a thread running led_fade_thread().
   Thread ledFadeThread(led_fade_thread);
   
@@ -87,13 +109,14 @@ int main() {
 	// look at status of a thread
 	
 	value1 = putcharThread.get_state();
-	//value2 = putcharThread1.get_state();
-	serial.printf("putcharThread: %d |", (long) value1);
+	value2 = putcharThread1.get_state();
+	//serial.printf("putcharThread: %d |", (long) value1);
 	//serial.printf("putcharThread1 %d", (long) value2);
-	//serial.printf("putcharThread: ");
-	//print_thread_status(value1);
-	//serial.printf("putcharThread1: ");
-	//print_thread_status(value2);
+	
+	 serial.printf("putcharThread: ");
+	 print_thread_status(value1);
+	 serial.printf("putcharThread1: ");
+	 print_thread_status(value2);
 	
 	
 	//serial.printf("get_state() %d", (long) value);
